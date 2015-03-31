8b5421a7c52d5c1c5c4f781034cc9781afa981b7
GaetanCousin <cousin.gaetan@laposte.net>
2015-03-31 12:40:58 +0200

    ajout entite classe java

--- src/entity/Menu.java
+++ src/entity/Menu.java
@@ -1,13 +1,47 @@
-package entity;
+package projetBidon;
 
 public class Menu {
-	private String name;
 
-	public String getName() {
-		return name;
+	private int id_menu;
+	private String name_menu;
+	private double price_menu;
+	
+	
+	public int getId_menu() {
+		return id_menu;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+
+	public void setId_menu(int id_menu) {
+		this.id_menu = id_menu;
 	}
+
+
+	public String getName_menu() {
+		return name_menu;
+	}
+
+
+	public void setName_menu(String name_menu) {
+		this.name_menu = name_menu;
+	}
+
+
+	public double getPrice_menu() {
+		return price_menu;
+	}
+
+
+	public void setPrice_menu(double price_menu) {
+		this.price_menu = price_menu;
+	}
+
+
+	public Menu(int id_menu, String name_menu, double price_menu) {
+		super();
+		this.id_menu = id_menu;
+		this.name_menu = name_menu;
+		this.price_menu = price_menu;
+	}
+		
 }
